8d6f26c7b0e70b8f97fd59404c9e1f7e24164c32
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2023-01-29 23:16:25 -0300

    blog_interna

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/barbo/precache-manifest.7d204b3f3b254c16815442917bfd3bca.js"
+  "/barbo/empreendimentos_interna/precache-manifest.6d706b4ac0551d186c9f16c4a46dcbc2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/barbo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/barbo/empreendimentos_interna/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
