366b3003221457ab69240e65d0742bd711951965
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2023-03-07 12:21:41 -0300

    quem_somos

--- src/Routes.js
+++ src/Routes.js
@@ -39,10 +39,10 @@ const Routes = () => (
     <BrowserRouter>
         <Switch>
             {/* <Route path='/barbo/fale_conosco' component={FaleConosco_Page} />
-            <Route path='/barbo/portal_corretor' component={PortalCorretor_Page} />
-             <Route path='/barbo/quem_somos' component={QuemSomos_Page} /> */}
+            <Route path='/barbo/portal_corretor' component={PortalCorretor_Page} />*/}
+             <Route path='/barbo/quem_somos' component={QuemSomos_Page} /> 
 
-            <Route path='/barbo/blog' component={Blog_Page} />
+            {/* <Route path='/barbo/blog' component={Blog_Page} /> */}
              {/*<Route path='/barbo/declarar_imposto' component={DeclararImposto} />
             <Route path='/barbo/beneficios_plantas' component={BeneficiosPlantas} />*/}
             {/* <Route path='/barbo/iluminacao' component={BlogIluminacao} /> */}
