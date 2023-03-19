a770fa2c278b28e93e117b7b43f1ef5341d6f1c9
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2023-03-19 18:52:27 -0300

    beneficios_plantas

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/empreendimentos/precache-manifest.518736129d0596cf728b77ceb4799ebc.js"
+  "/iluminacao/precache-manifest.589ea5640f83e7dfc0ab4765ee9a21b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/empreendimentos/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iluminacao/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- src/Routes.js
+++ src/Routes.js
@@ -44,8 +44,8 @@ const Routes = () => (
 
       {/* <Route path="/blog" component={Blog_Page} /> */}
       {/* <Route path="/declarar_imposto" component={DeclararImposto} /> */}
-      {/* <Route path="/beneficios_plantas" component={BeneficiosPlantas} /> */}
-      <Route path="/iluminacao" component={BlogIluminacao} />
+      <Route path="/beneficios_plantas" component={BeneficiosPlantas} />
+      {/* <Route path="/iluminacao" component={BlogIluminacao} /> */}
 
       {/* <Route path="/empreendimentos" component={Empreendimentos_Page} />  */}
       {/* <Route path="/office_tower" component={OfficeTower} /> */}
