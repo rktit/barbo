b100a836bd4ddade3ddd6c7d8d1dc04b28376aac
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2023-03-26 12:08:04 -0300

    portal_corretor

--- src/Routes.js
+++ src/Routes.js
@@ -39,8 +39,8 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       {/* <Route path="/barbo/fale_conosco" component={FaleConosco_Page} /> */}
-      {/* <Route path="/barbo/portal_corretor" component={PortalCorretor_Page} /> */}
-      <Route path="/barbo/quem_somos" component={QuemSomos_Page} />
+      <Route path="/barbo/portal_corretor" component={PortalCorretor_Page} />
+      {/* <Route path="/barbo/quem_somos" component={QuemSomos_Page} /> */}
 
       {/* <Route path="/barbo/blog" component={Blog_Page} /> */}
       {/* <Route path="/barbo/declarar_imposto" component={DeclararImposto} /> */}
